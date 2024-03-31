8f2fce64194f6295085cec4f8ba30b9c11a8d326
Nneli Ifeanyi <Nneli Ifeanyi>
2024-03-31 14:46:48 +0100

    rm bug

--- app/views/inc/footer.php
+++ app/views/inc/footer.php
@@ -74,11 +74,3 @@
 </script>
 </html>
 
-https://code.jquery.com/jquery-3.7.1.js
-https://cdn.datatables.net/2.0.3/js/dataTables.js
-https://cdn.datatables.net/buttons/3.0.1/js/dataTables.buttons.js
-https://cdn.datatables.net/buttons/3.0.1/js/buttons.dataTables.js
-https://cdnjs.cloudflare.com/ajax/libs/jszip/3.10.1/jszip.min.js
-https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.2.7/pdfmake.min.js
-https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.2.7/vfs_fonts.js
-https://cdn.datatables.net/buttons/3.0.1/js/buttons.html5.min.js
