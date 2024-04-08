8ac3b1fe0ea6e2027fe13ee249ffdb76706c89c1
Nneli Ifeanyi <Nneli Ifeanyi>
2024-04-08 01:27:47 +0100

    and so

--- app/views/journal/index.php
+++ app/views/journal/index.php
@@ -19,13 +19,11 @@
         <a class="btn btn-primary pull-right" href="<?php echo URLROOT; ?>/journal/add"><i class="fa fa-pencil" aria-hidden="true"></i> Add Post</a>
       </div>
     </div>
-    <?php foreach($data['posts'] as $post) : 
-      $post_time = strtotime($post->created_at);
-    ?>
+    <?php foreach($data['posts'] as $post):?>
       <div class="card card-body mb-3">
         <h4 class="card-title"><?php echo $post->title; ?></h4>
         <div class="bg-light p-2 mb-3">
-          Written <?php echo to_time_ago($post_time); ?>
+          Written <?php echo $post->created_at; ?>
         </div>
         <p class="card-text"><?php echo $post->body; ?></p>
         <a class="btn btn-dark" href="<?php echo URLROOT; ?>/journal/show/<?php echo $post->postId; ?>">More</a>
